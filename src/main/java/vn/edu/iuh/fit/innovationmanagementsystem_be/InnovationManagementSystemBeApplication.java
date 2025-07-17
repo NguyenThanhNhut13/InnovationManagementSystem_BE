58ddd75a0ff7a17d2cfbf7c8bfff029bb81cc5ee
NguyenThanhNhut13 <thanhnhutcu@gmail.com>
2025-07-17 15:15:22 +0700

    init project

---
+++ src/main/java/vn/edu/iuh/fit/innovationmanagementsystem_be/InnovationManagementSystemBeApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.innovationmanagementsystem_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovationManagementSystemBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovationManagementSystemBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/edu/iuh/fit/innovationmanagementsystem_be/InnovationManagementSystemBeApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.innovationmanagementsystem_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovationManagementSystemBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
