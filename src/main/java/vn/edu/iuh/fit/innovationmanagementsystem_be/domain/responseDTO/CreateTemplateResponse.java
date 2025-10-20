23c635f2537a21f33171d7abed21b0b684e7bed4
NguyenTongAnhQuan443 <ntanhquan.sly@gmail.com>
2025-10-20 09:36:47 +0700

    Update Template DTO

---
+++ src/main/java/vn/edu/iuh/fit/innovationmanagementsystem_be/domain/responseDTO/CreateTemplateResponse.java
@@ -0,0 +1,44 @@
+package vn.edu.iuh.fit.innovationmanagementsystem_be.domain.responseDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import vn.edu.iuh.fit.innovationmanagementsystem_be.domain.model.enums.FieldTypeEnum;
+import vn.edu.iuh.fit.innovationmanagementsystem_be.domain.model.enums.TemplateTypeEnum;
+import vn.edu.iuh.fit.innovationmanagementsystem_be.domain.model.enums.TargetRoleCode;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateTemplateResponse {
+
+    private String id;
+    private String templateContent;
+    private TemplateTypeEnum templateType;
+    private TargetRoleCode targetRole;
+    private String roundId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String createdBy;
+    private String updatedBy;
+    private List<FieldResponse> fields;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class FieldResponse {
+        private String id;
+        private String fieldKey;
+        private String label;
+        private FieldTypeEnum type;
+        private Boolean required;
+        private String placeholder;
+        private Object tableConfig;
+        private Object options;
+        private Boolean repeatable;
+        private List<FieldResponse> children;
+    }
+}
