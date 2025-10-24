175733cdbd46f0ea88a1ff70c84590be46ac1cd6
NguyenTongAnhQuan443 <ntanhquan.sly@gmail.com>
2025-10-24 16:05:00 +0700

    update

--- src/main/java/vn/edu/iuh/fit/innovationmanagementsystem_be/mapper/FormTemplateMapper.java
+++ src/main/java/vn/edu/iuh/fit/innovationmanagementsystem_be/mapper/FormTemplateMapper.java
@@ -2,33 +2,24 @@ package vn.edu.iuh.fit.innovationmanagementsystem_be.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 import vn.edu.iuh.fit.innovationmanagementsystem_be.domain.model.FormTemplate;
 import vn.edu.iuh.fit.innovationmanagementsystem_be.domain.responseDTO.FormTemplateResponse;
 import vn.edu.iuh.fit.innovationmanagementsystem_be.domain.responseDTO.CreateTemplateResponse;
 import vn.edu.iuh.fit.innovationmanagementsystem_be.domain.responseDTO.CreateTemplateWithFieldsResponse;
-import vn.edu.iuh.fit.innovationmanagementsystem_be.utils.Base64Utils;
 
 @Mapper(componentModel = "spring", uses = { FormFieldMapper.class })
 public interface FormTemplateMapper {
 
     @Mapping(target = "innovationRoundId", source = "innovationRound.id")
     @Mapping(target = "innovationRoundName", source = "innovationRound.name", defaultValue = "")
-    @Mapping(target = "templateContent", source = "templateContent", qualifiedByName = "decodeTemplateContent")
     FormTemplateResponse toFormTemplateResponse(FormTemplate formTemplate);
 
     @Mapping(target = "roundId", source = "innovationRound.id")
     @Mapping(target = "fields", source = "formFields")
-    @Mapping(target = "templateContent", source = "templateContent", qualifiedByName = "decodeTemplateContent")
     CreateTemplateResponse toCreateTemplateResponse(FormTemplate formTemplate);
 
     @Mapping(target = "roundId", source = "innovationRound.id")
     @Mapping(target = "fields", source = "formFields")
-    @Mapping(target = "templateContent", source = "templateContent", qualifiedByName = "decodeTemplateContent")
     CreateTemplateWithFieldsResponse toCreateTemplateWithFieldsResponse(FormTemplate formTemplate);
 
-    @Named("decodeTemplateContent")
-    default String decodeTemplateContent(String encodedContent) {
-        return Base64Utils.decode(encodedContent);
-    }
 }
