58353ab3a52853037e36bbddf060b5d5b31fe345
NguyenTongAnhQuan443 <ntanhquan.sly@gmail.com>
2025-10-25 20:53:22 +0700

    Update Role Permission 1

---
+++ src/main/java/vn/edu/iuh/fit/innovationmanagementsystem_be/config/PermissionConstants.java
@@ -0,0 +1,19 @@
+package vn.edu.iuh.fit.innovationmanagementsystem_be.config;
+
+public class PermissionConstants {
+
+        public static final String[] PUBLIC = {
+                        "/api/v1/auth/login",
+                        "/api/v1/auth/refresh",
+                        "/api/v1/auth/forgot-password",
+                        "/api/v1/auth/reset-password",
+                        "/api/v1/utils/ping",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html",
+                        "/v3/api-docs/**",
+                        "/api-docs/**",
+                        "/swagger-resources/**",
+                        "/webjars/**",
+                        "/api/v1/users" // Method POST
+        };
+}
--- src/main/java/vn/edu/iuh/fit/innovationmanagementsystem_be/config/SecurityConfig.java
+++ src/main/java/vn/edu/iuh/fit/innovationmanagementsystem_be/config/SecurityConfig.java
@@ -1,11 +1,9 @@
 package vn.edu.iuh.fit.innovationmanagementsystem_be.config;
 
-import vn.edu.iuh.fit.innovationmanagementsystem_be.domain.model.enums.UserRoleEnum;
 import vn.edu.iuh.fit.innovationmanagementsystem_be.exception.CustomAccessDeniedHandler;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -21,20 +19,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
-
-        private final String GIANG_VIEN = UserRoleEnum.GIANG_VIEN.name();
-        // private final String QUAN_TRI_VIEN_KHOA =
-        // UserRoleEnum.QUAN_TRI_VIEN_KHOA.name();
-        private final String TRUONG_KHOA = UserRoleEnum.TRUONG_KHOA.name();
-        // private final String TV_HOI_DONG_KHOA = UserRoleEnum.TV_HOI_DONG_KHOA.name();
-        private final String QUAN_TRI_VIEN_QLKH_HTQT = UserRoleEnum.QUAN_TRI_VIEN_QLKH_HTQT.name();
-        // private final String TV_HOI_DONG_TRUONG =
-        // UserRoleEnum.TV_HOI_DONG_TRUONG.name();
-        // private final String CHU_TICH_HD_TRUONG =
-        // UserRoleEnum.CHU_TICH_HD_TRUONG.name();
-        private final String QUAN_TRI_VIEN_HE_THONG = UserRoleEnum.QUAN_TRI_VIEN_HE_THONG.name();
 
         private final JwtBlacklistFilter jwtBlacklistFilter;
         private final JwtDecoder jwtDecoder;
@@ -57,41 +43,8 @@ public class SecurityConfig {
                                 .csrf(csrf -> csrf.disable())
                                 .cors(Customizer.withDefaults())
                                 .authorizeHttpRequests(auth -> auth
-
-                                                // Public endpoints
-                                                .requestMatchers(EndpointConstants.PUBLIC).permitAll()
-
-                                                // Role Management
-                                                .requestMatchers(HttpMethod.POST,
-                                                                EndpointConstants.ROLE_MANAGEMENT_POST)
-                                                .hasAnyRole(QUAN_TRI_VIEN_HE_THONG, QUAN_TRI_VIEN_QLKH_HTQT,
-                                                                TRUONG_KHOA)
-                                                .requestMatchers(HttpMethod.DELETE,
-                                                                EndpointConstants.ROLE_MANAGEMENT_DELETE)
-                                                .hasAnyRole(QUAN_TRI_VIEN_HE_THONG, QUAN_TRI_VIEN_QLKH_HTQT,
-                                                                TRUONG_KHOA)
-
-                                                // Department
-                                                .requestMatchers(HttpMethod.GET, EndpointConstants.DEPARTMENT_GET)
-                                                .hasAnyRole(QUAN_TRI_VIEN_HE_THONG, QUAN_TRI_VIEN_QLKH_HTQT)
-
-                                                // Innovation Statistics
-                                                .requestMatchers(HttpMethod.GET, EndpointConstants.INNOVATION_GET)
-                                                .hasRole(GIANG_VIEN)
-
-                                                // Innovation Round
-                                                .requestMatchers(HttpMethod.POST,
-                                                                EndpointConstants.INNOVATION_ROUND_POST)
-                                                .hasAnyRole(QUAN_TRI_VIEN_HE_THONG, QUAN_TRI_VIEN_QLKH_HTQT)
-                                                .requestMatchers(HttpMethod.PUT, EndpointConstants.INNOVATION_ROUND_PUT)
-                                                .hasAnyRole(QUAN_TRI_VIEN_HE_THONG, QUAN_TRI_VIEN_QLKH_HTQT)
-
-                                                // Form Template
-                                                .requestMatchers(HttpMethod.POST, EndpointConstants.FORM_TEMPLATE_POST)
-                                                .hasAnyRole(QUAN_TRI_VIEN_HE_THONG, QUAN_TRI_VIEN_QLKH_HTQT)
-                                                .requestMatchers(HttpMethod.PUT, EndpointConstants.FORM_TEMPLATE_PUT)
-                                                .hasAnyRole(QUAN_TRI_VIEN_HE_THONG, QUAN_TRI_VIEN_QLKH_HTQT)
-
+                                                // Chỉ cho phép public endpoints không cần đăng nhập
+                                                .requestMatchers(PermissionConstants.PUBLIC).permitAll()
                                                 .anyRequest().authenticated())
                                 .oauth2ResourceServer(oauth2 -> oauth2
                                                 .jwt(jwt -> jwt
