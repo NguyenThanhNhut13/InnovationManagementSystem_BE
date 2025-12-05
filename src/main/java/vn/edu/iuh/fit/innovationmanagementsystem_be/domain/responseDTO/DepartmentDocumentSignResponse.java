7986f00760fbb6281dc6b7a5ca86d0cd46d44e90
NguyenTongAnhQuan443 <ntanhquan.sly@gmail.com>
2025-12-06 02:53:33 +0700

    Update Request, Response Department Sign

--- src/main/java/vn/edu/iuh/fit/innovationmanagementsystem_be/domain/responseDTO/DepartmentDocumentSignResponse.java
+++ src/main/java/vn/edu/iuh/fit/innovationmanagementsystem_be/domain/responseDTO/DepartmentDocumentSignResponse.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import vn.edu.iuh.fit.innovationmanagementsystem_be.domain.model.enums.DocumentTypeEnum;
+import vn.edu.iuh.fit.innovationmanagementsystem_be.domain.model.enums.UserRoleEnum;
 
 import java.time.LocalDateTime;
 
@@ -12,11 +13,15 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class DepartmentDocumentSignResponse {
 
+    private String reportId;
+    private Boolean isSigned;
     private String signatureId;
     private String documentHash;
     private String signatureHash;
     private DocumentTypeEnum documentType;
     private String departmentId;
+    private String councilId;
+    private UserRoleEnum signedAsRole;
     private String signerName;
     private LocalDateTime signedAt;
     private String pdfUrl;
