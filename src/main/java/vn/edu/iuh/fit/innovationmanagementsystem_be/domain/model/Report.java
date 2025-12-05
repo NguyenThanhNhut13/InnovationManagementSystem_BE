725f01f328da0b3342eac67363cf8ab2659f8817
NguyenTongAnhQuan443 <ntanhquan.sly@gmail.com>
2025-12-06 00:02:59 +0700

    Remove Report Innovation Details

--- src/main/java/vn/edu/iuh/fit/innovationmanagementsystem_be/domain/model/Report.java
+++ src/main/java/vn/edu/iuh/fit/innovationmanagementsystem_be/domain/model/Report.java
@@ -2,14 +2,18 @@ package vn.edu.iuh.fit.innovationmanagementsystem_be.domain.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import java.time.LocalDateTime;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import vn.edu.iuh.fit.innovationmanagementsystem_be.domain.model.enums.DocumentTypeEnum;
-import java.util.List;
+
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "reports")
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Report extends Auditable {
@@ -19,26 +23,17 @@ public class Report extends Auditable {
     @Column(name = "id", columnDefinition = "VARCHAR(36)")
     private String id;
 
-    @Column(name = "applicable_year", nullable = false, columnDefinition = "INTEGER")
-    private Integer applicableYear;
-
-    @Column(name = "generated_date", nullable = false, columnDefinition = "TIMESTAMP")
-    private LocalDateTime generatedDate;
+    @Column(name = "department_id", nullable = false, columnDefinition = "VARCHAR(36)")
+    private String departmentId;
 
     @Column(name = "generated_pdf_path", columnDefinition = "TEXT")
     private String generatedPdfPath;
-
-    @Column(name = "meeting_details", nullable = false, columnDefinition = "TEXT")
-    private String meetingDetails;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "report_type", columnDefinition = "VARCHAR(50)")
     private DocumentTypeEnum reportType;
 
     // Relationships
-    @OneToMany(mappedBy = "report", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
-    private List<ReportInnovationDetail> reportInnovationDetails = new ArrayList<>();
-
     @OneToMany(mappedBy = "report", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = false)
     private List<DigitalSignature> digitalSignatures = new ArrayList<>();
 }
